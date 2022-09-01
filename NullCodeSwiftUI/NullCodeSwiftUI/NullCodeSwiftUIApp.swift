a1c1d6f9c505b4a075303b1540d47ec8ca0bd8f6
jyang <jTang@walkgame.com>
2022-09-01 15:24:50 +0800

    init project

---
+++ NullCodeSwiftUI/NullCodeSwiftUI/NullCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NullCodeSwiftUIApp.swift
+//  NullCodeSwiftUI
+//
+//  Created by Apple on 2022/9/1.
+//
+
+import SwiftUI
+
+@main
+struct NullCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
